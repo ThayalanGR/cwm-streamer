5d55c02d386252cdd03904d9dcad20c3462f8109
Devaraj <nsdevaraj@gmail.com>
2024-09-13 18:11:08 +0530

    fix port

--- client/src/config/authConfig.ts
+++ client/src/config/authConfig.ts
@@ -4,7 +4,7 @@ export const msalConfig: Configuration = {
     auth: {
         clientId: "d10723ca-4573-46a3-b1c5-36afa36d18e3", // Replace with your Azure AD app's client ID
         authority: "https://login.microsoftonline.com/9f84ee4b-bee3-42f7-b84c-e57e5e92f105", // Replace with your Azure AD tenant ID
-        redirectUri: "http://localhost:3000" // Replace with your app's redirect URI
+        redirectUri: "http://localhost:5173" // Replace with your app's redirect URI
     },
     cache: {
         cacheLocation: "sessionStorage",
